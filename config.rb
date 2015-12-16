41209a7f8beae5dea3c32d9ec595dfa594648a8b
yocky96 <yocky96@users.noreply.github.com>
2015-12-16 10:27:43 +0100

    Update config.rb

--- config.rb
+++ config.rb
@@ -2,10 +2,10 @@
 module Config
 	DEFAULT = {
 	:client_id 		=> YOURCLIENTID,
-	:client_secret 	=> YOURCLIENTSECRET,
+	:client_secret 		=> YOURCLIENTSECRET,
 	:username 		=> YOURUSERNAME,
 	:password 		=> YOURPASSWORD,
-	:playlist_name	=> SYNC_PLAYLIST,
+	:playlist_name		=> SYNC_PLAYLIST,
 	:page_size		=> MAX_PAGESIZE
 	}
 end
